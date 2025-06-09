bd9556226cc1532fba3660bc6ff546c9d6465715
upyadushkina <upyadushkina@gmail.com>
2025-06-09 17:14:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.image("heading.png", use_container_width=True)
 # st.markdown("Select a year below to see which mosques existed at that time.")
 
 # Ползунок времени
-year = st.slider("", min_value=int(df['decade_built'].min()), max_value=2025), value=1700, step=10)
+year = st.slider("", min_value=int(df['decade_built'].min()), max_value=2025, value=1700, step=10)
 
 # Фильтрация мечетей по году существования
 mask = (df['decade_built'] <= year) & ((df['decade_demolished'].isna()) | (df['decade_demolished'] >= year))
